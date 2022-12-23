32e9020941bf9e8e1b8efea4babc832751c1efde
Jonas-Victor950 <111525829+Jonas-Victor950@users.noreply.github.com>
2022-12-23 20:10:32 -0300

    tsc build

--- Back-end1/build/database/app.js
+++ Back-end1/build/database/app.js
@@ -4,5 +4,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const Conection_1 = __importDefault(require("./Conection"));
-const mongoDB = new Conection_1.default("mongodb://localhost:27017/dindin");
+const mongoose_1 = __importDefault(require("mongoose"));
+const default_1 = __importDefault(require("./default"));
+mongoose_1.default.connect(`mongodb+srv://${default_1.default.user}:${default_1.default.pass}@cluster0.mbitszy.mongodb.net/?retryWrites=true&w=majority`);
+const mongoDB = new Conection_1.default(`mongodb+srv://${default_1.default.user}:${default_1.default.pass}@cluster0.mbitszy.mongodb.net/?retryWrites=true&w=majority`);
 exports.default = mongoDB;
--- Back-end1/build/database/default.js
+++ Back-end1/build/database/default.js
@@ -1,11 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const authDB = {
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    database: process.env.DB_NAME,
     user: process.env.DB_USER,
     pass: process.env.DB_PASS,
-    env: "development",
 };
 exports.default = authDB;
